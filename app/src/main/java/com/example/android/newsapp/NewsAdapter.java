5ebbac27798fbf833d357bc25ca9dadf78356cc5
SilentOne <SilentOne@onet.pl>
2018-07-04 21:32:11 +0200

    Fetching data via network request. AsyncTask

---
+++ app/src/main/java/com/example/android/newsapp/NewsAdapter.java
@@ -0,0 +1,59 @@
+package com.example.android.newsapp;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * An {@link NewsAdapter} knows how to create a list item layout for each article
+ * in the data source (a list of {@link News} objects).
+ *
+ * These list item layouts will be provided to an adapter view like ListView
+ * to be displayed to the user.
+ */
+public class NewsAdapter extends ArrayAdapter<News> {
+
+    /**
+     * Constructs a new {@link NewsAdapter}.
+     * @param context of the app
+     * @param news is the list of articles, which is the data source of the adapter
+     */
+    public NewsAdapter(Context context, List<News> news) {
+        super(context, 0, news);
+    }
+
+    /**
+     * Returns a list item view that displays information about the earthquake at the given position
+     * in the list of articles.
+     */
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // Check if there is any existing list item view (called convertView) that we can reuse,
+        // otherwise, if convertView is null, then inflate a new list item layout.
+        View listItemView = convertView;
+        if (listItemView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(
+                    R.layout.list_item,parent,false);
+        }
+
+        // Find the article at the given position in the list of articles
+        News currentNews = getItem(position);
+
+        // Find the TextView with view ID title
+        TextView titleView = listItemView.findViewById(R.id.title);
+        // Display the title of the current article in that TextView
+        titleView.setText(currentNews.getTitle());
+
+        // Find the TextView with view ID author
+        TextView authorView = listItemView.findViewById(R.id.author);
+        // Display the author of the current article in that TextView
+        authorView.setText(currentNews.getAuthor());
+
+        return listItemView;
+    }
+}
---
+++ app/src/main/java/com/example/android/newsapp/QueryUtils.java
@@ -0,0 +1,186 @@
+package com.example.android.newsapp;
+
+import android.text.TextUtils;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Helper methods related to requesting and receiving articles data from GUARDIAN.
+ */
+public final class QueryUtils {
+
+    /** Tag for the log messages */
+    private static final String LOG_TAG = QueryUtils.class.getSimpleName();
+
+    /**
+     * Create a private constructor because no one should ever create a {@link QueryUtils} object.
+     * This class is only meant to hold static variables and methods, which can be accessed
+     * directly from the class name QueryUtils (and an object instance of QueryUtils is not needed).
+     */
+    private QueryUtils() {
+    }
+
+    /**
+     * Query the GUARDIAN dataset and return a list of {@link News} objects.
+     */
+    public static List<News> fetchNewsData(String requestUrl) {
+        // Create URL object
+        URL url = createUrl(requestUrl);
+
+        // Perform HTTP request to the URL and receive a JSON response back
+        String jsonResponse = null;
+        try {
+            jsonResponse = makeHttpRequest(url);
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "Problem making HTTP request.", e);
+        }
+
+        // Extract relevant fields from the JSON response and create a list of {@link News}
+        List<News> news = extractFeatureFromJson(jsonResponse);
+
+        // Return the list of {@link News}
+        return news;
+    }
+
+    /**
+     * Returns new URL object rom the given string URL.
+     */
+    private static URL createUrl(String stringURL) {
+        URL url = null;
+        try {
+            url = new URL(stringURL);
+        } catch (MalformedURLException e) {
+            Log.e(LOG_TAG, "Problem building the URL", e);
+        }
+        return url;
+    }
+
+    /**
+     * Make an HTTP request to the given URL and return a String as the response.
+     */
+    private static String makeHttpRequest(URL url) throws IOException {
+        String jsonResponse = " ";
+
+        // If the URL is null, then return early.
+        if (url == null) {
+            return jsonResponse;
+        }
+
+        HttpURLConnection urlConnection = null;
+        InputStream inputStream = null;
+        try {
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setReadTimeout(10000 /* milliseconds */);
+            urlConnection.setConnectTimeout(15000 /* milliseconds */);
+            urlConnection.setRequestMethod("GET");
+            urlConnection.connect();
+
+            // If the request was successful (response code 200),
+            // then read the input stream and parse the response.
+            if (urlConnection.getResponseCode() == 200) {
+                inputStream = urlConnection.getInputStream();
+                jsonResponse = readFromStream(inputStream);
+            } else {
+                Log.e(LOG_TAG, "Error response code: " + urlConnection.getResponseCode());
+            }
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "Problem retrieving the earthquake JSON results.", e);
+        } finally {
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
+            if (inputStream != null) {
+                // Closing the input stream could throw an IOException, which is why
+                // the makeHttpRequest(URL url) method signature specifies than an IOException
+                // could be thrown.
+                inputStream.close();
+            }
+        }
+        return jsonResponse;
+    }
+
+    /**
+     * Conver the {@link InputStream} into a String which contains
+     * the whole JSON response from the server.
+     */
+    private static String readFromStream(InputStream inputStream) throws IOException {
+        StringBuilder output = new StringBuilder();
+        if (inputStream != null) {
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream, Charset.forName("UTF-8"));
+            BufferedReader reader = new BufferedReader(inputStreamReader);
+            String line = reader.readLine();
+            while (line != null) {
+                output.append(line);
+                line = reader.readLine();
+            }
+        }
+
+        return output.toString();
+    }
+
+    /**
+     * Return a list of {@link News} objects that has been built up
+     * from parsing the given JSON response.
+     */
+    private static List<News> extractFeatureFromJson(String newsJSON) {
+        // If the JSON string is empty or null, then return early.
+        if (TextUtils.isEmpty(newsJSON))
+            return null;
+
+        // Create an empty ArrayList that we can start adding articles to
+        List<News> news = new ArrayList<>();
+
+        // Try to parse the JSON response string. If there's a problem with the way the JSON
+        // is formatted, a JSONException exception object will be thrown.
+        // Catch the exception so the app doesn't crash and print the error message to the logs.
+        try {
+            JSONObject baseJsonResponse = new JSONObject(newsJSON);
+            JSONObject jsonTest = baseJsonResponse.getJSONObject("response");
+
+            JSONArray results = jsonTest.getJSONArray("results");
+
+            // Looping through all objects
+            for (int i = 0; i < results.length(); i++) {
+                JSONObject c = results.getJSONObject(i);
+                String title = c.getString("webTitle");
+
+                JSONArray tags = c.getJSONArray("tags");
+                JSONObject obj = tags.getJSONObject(0);
+                String authorName = obj.optString("firstName");
+                String authorLastName = obj.optString("lastName");
+
+                StringBuilder builder = new StringBuilder();
+                builder.append(authorName).append(" ").append(authorLastName);
+                String fullName = builder.toString().toUpperCase();
+
+                // Create a new {@link News} object with the title and author from the JSON response.
+                News article = new News(title, fullName);
+
+                // Add the new {@link News} to the list of articles.
+                news.add(article);
+            }
+        } catch (JSONException e) {
+            // If an error is thrown when executing any of the above statements in the "try" block,
+            // catch the exception here, so the app doesn't crash. Print a log message
+            // with the message from the exception.
+            Log.e("QueryUtils", "Problm parsing the earthquake JSON results", e);
+        }
+
+        // Return the list of articles
+        return news;
+    }
+}
